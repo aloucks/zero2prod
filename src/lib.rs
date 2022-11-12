913865eb3d6679c130bee7e7368355afe2c372b4
Aaron Loucks <aloucks@cofront.net>
2022-11-12 14:22:00 -0500

    Updates tests for postgres

--- src/lib.rs
+++ src/lib.rs
@@ -1,36 +1,3 @@
-use std::net::TcpListener;
-
-use actix_web::{dev::Server, web, App, HttpResponse, HttpServer, Responder};
-use serde::Deserialize;
-
-async fn health_check() -> impl Responder {
-    HttpResponse::Ok()
-}
-
-async fn subscribe() -> HttpResponse {
-    HttpResponse::Ok().finish()
-}
-
-#[derive(Deserialize)]
-struct FormData {
-    email: String,
-    name: String,
-}
-/// Extract form data using serde.
-/// This handler get called only if content type is *x-www-form-urlencoded*
-/// and content of the request could be deserialized to a `FormData` struct
-fn index(form: web::Form<FormData>) -> String {
-    format!("Welcome {}!", form.name)
-}
-
-pub fn run(listener: TcpListener) -> Result<Server, std::io::Error> {
-    let server = HttpServer::new(|| {
-        App::new()
-            .route("/health_check", web::get().to(health_check))
-            .route("/subscriptions", web::post().to(subscribe))
-    })
-    .listen(listener)?
-    .run();
-
-    Ok(server)
-}
+pub mod configuration;
+pub mod routes;
+pub mod startup;
---
+++ src/routes/subscriptions.rs
@@ -0,0 +1,37 @@
+use actix_web::{web, HttpResponse};
+use serde::Deserialize;
+use sqlx::{
+    types::{chrono::Utc, Uuid},
+    PgPool,
+};
+
+pub async fn subscribe(
+    form: web::Form<FormData>,
+    connection_pool: web::Data<PgPool>,
+) -> HttpResponse {
+    let query = sqlx::query!(
+        r#"
+        INSERT INTO subscriptions (id, email, name, subscribed_at)
+        VALUES ($1, $2, $3, $4)
+        "#,
+        Uuid::new_v4(),
+        form.email,
+        form.name,
+        Utc::now()
+    )
+    .execute(connection_pool.get_ref());
+
+    match query.await {
+        Ok(_) => HttpResponse::Ok().finish(),
+        Err(e) => {
+            println!("Failed to execute query: {}", e);
+            HttpResponse::InternalServerError().finish()
+        }
+    }
+}
+
+#[derive(Deserialize)]
+pub struct FormData {
+    email: String,
+    name: String,
+}
