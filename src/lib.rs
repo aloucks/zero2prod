4cc83321918d623a13fe45a684e8b1172e79d66e
Aaron Loucks <aloucks@cofront.net>
2022-11-12 12:21:37 -0500

    Restructure health_check test

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,6 @@
-use actix_web::{web, App, HttpRequest, HttpResponse, HttpServer, Responder, dev::Server};
+use std::net::TcpListener;
+
+use actix_web::{dev::Server, web, App, HttpRequest, HttpResponse, HttpServer, Responder};
 
 async fn greet(req: HttpRequest) -> impl Responder {
     let name = req.match_info().get("name").unwrap_or("World");
@@ -9,14 +11,14 @@ async fn health_check() -> impl Responder {
     HttpResponse::Ok()
 }
 
-pub fn run() -> Result<Server, std::io::Error> {
+pub fn run(listener: TcpListener) -> Result<Server, std::io::Error> {
     let server = HttpServer::new(|| {
         App::new()
             .route("/health_check", web::get().to(health_check))
             .route("/", web::get().to(greet))
             .route("/{name}", web::get().to(greet))
     })
-    .bind("127.0.0.1:8000")?
+    .listen(listener)?
     .run();
 
     Ok(server)
